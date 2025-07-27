fcd04e53d734fcf952a3e5ff5e5987ed9af48568
Gersoncs107 <gerson.silva107@gmail.com>
2025-07-26 22:20:45 -0300

    phonebook-update

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.get('/info', (request, response) => {
 app.post('/api/persons', (request, response) => {
     const body = request.body
 
-    if(!body.name){
+    if(!body.name || !body.number){
         return response.status(404).json({
             error:'content missing'
         })
