1af04bd059cc6738ad6b29f381676d8ee24bcee5
Gersoncs107 <gerson.silva107@gmail.com>
2025-08-21 22:16:41 -0300

    phonebook-backend-update

--- models/person.js
+++ models/person.js
@@ -13,3 +13,8 @@ mongoose.connect(url)
   .catch((error) => {
     console.log('error connecting to MongoDB:', error.message)
   })
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String, 
+})
