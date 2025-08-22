aeaa48aaa02ced316e5008b809a7023141d2b3ec
Gersoncs107 <gerson.silva107@gmail.com>
2025-08-21 22:17:09 -0300

    phonebook-backend-update

--- models/person.js
+++ models/person.js
@@ -18,3 +18,11 @@ const personSchema = new mongoose.Schema({
     name: String,
     number: String, 
 })
+
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
